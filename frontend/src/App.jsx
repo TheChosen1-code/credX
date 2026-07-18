2d2b9687f6c4a75865e2b7964f7a96eaa0dd69c3
Anumanya Jaiswal <pr.anumanya.2648@gmail.com>
2026-07-18 11:38:12 +0530

    Routes Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Pages from "./index";
+import "./App.css";
 
 function App() {
-  return <CompanyDashboard />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
+      <Route path="/login" element={<Pages.Login />} />
+
+      <Route path="/signup" element={<Pages.Signup />} />
+
+      <Route
+        path="/company-dashboard"
+        element={<Pages.CompanyDashboard />}
+      />
+    </Routes>
+  );
 }
 
 export default App;
