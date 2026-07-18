343edf7231ed8500b656db9fe6057cebd90d13b8
Anumanya Jaiswal <pr.anumanya.2648@gmail.com>
2026-07-18 13:35:07 +0530

    Fixed bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,12 @@ import Signup from "./pages/Signup";
 import Login from './pages/Login'
 import CompanyDashboard from './pages/CompanyDashboard'
 import AdminDashboard from "./pages/AdminDashboard";
+import StudentDashboard from "./pages/StudentDashboard"
 
 export default {
     Signup,
     Login,
     CompanyDashboard,
-    AdminDashboard
+    AdminDashboard,
     StudentDashboard,
 }
